1298865b3bb69f22c082ae318bb1365eaa47d506
bobrandy13 <khuang891@gmail.com>
2024-06-10 15:41:23 +1000

    created basic models and updated requrements

---
+++ Models/Consumer.cs
@@ -0,0 +1,8 @@
+namespace Kafka_for_web.Models; 
+
+public class Consumer {
+    private long id;
+    private string name;
+
+    private long offset; 
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,8 @@
+namespace Kafka_for_web.Models; 
+
+public class Producer {
+    private long id;
+    private string name;
+
+    private string recent_messages; 
+}
